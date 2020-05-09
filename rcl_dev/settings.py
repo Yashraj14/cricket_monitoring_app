fff939647f87b205360af909339c1087c513a6f5
Yashraj14 <38345562+Yashraj14@users.noreply.github.com>
2020-05-09 17:41:18 -0400

    Update settings.py

--- rcl_dev/settings.py
+++ rcl_dev/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '7de@pd$r6!=gjr5c=5c3%@2(dy$wrkv5tjho&1z2&$!^l4@@%='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rcltournaments.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
