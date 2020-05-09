407ab26e6acb8b875a27a539aace937934fa95f5
Yashraj14 <38345562+Yashraj14@users.noreply.github.com>
2020-05-09 17:56:57 -0400

    v1

--- rcl_dev/settings.py
+++ rcl_dev/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7de@pd$r6!=gjr5c=5c3%@2(dy$wrkv5tjho&1z2&$!^l4@@%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['rcltournaments.herokuapp.com','127.0.0.1']
 
 
 # Application definition
